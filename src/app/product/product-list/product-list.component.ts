40359e131de05eda4b5b32e5c2500cb7ae95f873
thiru <thiru1921@gmail.com>
2020-02-27 20:21:53 +0530

    hide coe

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -10,8 +10,8 @@ export class ProductListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    $('.event-header').css('display', 'none');
-    $('.footer').css('display', 'none');
+    // $('.event-header').css('display', 'none');
+    // $('.footer').css('display', 'none');
   }
 
 }
