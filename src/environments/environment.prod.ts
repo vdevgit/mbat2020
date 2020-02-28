5321e376e5a53157261c9f787903ab19aaae92fb
thiru <thiru1921@gmail.com>
2020-02-29 00:24:57 +0530

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
     domain: 'mbat.eu.auth0.com',
     clientId: '9c37O59Yv6w0j9MtmpW4yJg62SqmTdk6',
     clientSecret: 'fIJxGMNYO3s4wC0HKS8LWM9qfvfFYr43oGPJL4Gp7S4sebP8UmiXJkvMv-zuviZn'
-  }
+  },
+  validEmailDomain: ['gmail.com', 'xyz.com']
 };
