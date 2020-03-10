2c41be4581e27b4729184aba16901fe6d413b58e
thiru <thiru1921@gmail.com>
2020-03-10 22:33:39 +0530

    add staging env file

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  mbatServer: 'https://api.mbat.xyz/',
+  auth0: {
+    domain: 'mbat-staging.eu.auth0.com',
+    clientId: '50PT42OeP7iJr8FdzIt5r4zTJS3cglFZ',
+    clientSecret: 'HI8le1hd8pCao9t7Qsx5Ih_2LLzOKAuTQ5UK760tG6s9m-yTuJ2UlX0CNdPiXsnU'
+  },
+  ticketIframe: 'https://eu.explara.com/widget-new/mba-tournament',
+  validEmailDomain: ['@gmail.com', '@hec.edu','@edhec.com', '@mba.sdabocconi.it', '@cam.ac.uk', '@said.oxford.edu', '@postgrad.manchester.ac.uk', 'Ehansen.mba2020@london.edu', 'no reply', 'stephanie.hurry@mba2020.imd.org', '@ie.edu', '@insead.edu', '@cranfield.ac.uk', '@tias.edu', '@iese.net', 'no reply', '@imperial.ac.uk', '@unisg.ch', 'No contact available', '*', 'No contact available', 'Stuti has', 'Stuti has', '*', '@esmt.org', '@whu.edu', '363454738@qq.com', '@kingston.ac.uk', '@hhl.de', 'No contact available', '@student.bham.ac.uk', 'No contact available']
+};
+
+
+
+ 
