5ea52b7cc34bcbe7d99a4343084f6949f546e827
thiru <thiru1921@gmail.com>
2020-03-14 12:36:59 +0530

    submit contact us form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -6,17 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
+  contactUsForm = {}
   name: string;
   surname: string;
   schoolname: string;
   email: string;
   subject: string;
   message: string;
-
-  constructor() { }
+  visible = false;
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+  submitContactUs(event) {
+    console.log(this.name);
+    if ( this.name && this.surname && this.schoolname && this.email && this.subject && this.message) {
+      this.visible = true;
+      let data = { name: this.name, surname: this.surname, schoolname: this.schoolname, email: this.email, subject: this.subject, message: this.message }
+      this.http.post<any>("https://formspree.io/mknzorgk", data).subscribe(data => {
+        console.log(data);
+        this.visible = false;
+      });
+    }
   }
 
 }
