8a2a67997d64314ed437fafceedd60e6bfbda703
thiru <thiru1921@gmail.com>
2021-02-06 18:21:25 +0530

    send the data to email

--- src/app/sponsors/sponsors.component.ts
+++ src/app/sponsors/sponsors.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sponsors',
@@ -7,7 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SponsorsComponent implements OnInit {
 
-  constructor() { }
+  firstName: string;
+  lastName: string;
+  emailId: string;
+  message: string;
+  visible = false;
+  constructor(private http: HttpClient, public router: Router) { }
+
+  errorMessage: string;
+
+  validateSponsors(event: any) {
+    return ""
+  }
+  validatePartners(event: any) {
+    return ""
+  }
+  submitInterest() {
+    console.log(this.firstName);
+    if ( this.firstName && this.lastName && this.emailId && this.message) {
+      console.log(this.message);
+      this.visible = true;
+      let data = { fristName: this.firstName, lastName: this.lastName, emailId: this.emailId, message: this.message }
+      this.http.post<any>("https://formspree.io/mknzorgk", data).subscribe(data => {
+        console.log(data);
+        this.visible = false;
+        this.router.navigate(['/']);
+      });
+    }
+  }
 
   ngOnInit(): void {
   }
